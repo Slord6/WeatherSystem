50febdd281d514d0cd7028f88db4c3c6567470d2
Sam Lord <samuel.lord@students.plymouth.ac.uk>
2018-02-12 15:14:57 +0000

    Fix generator compile error

--- WeatherSystem/Assets/WeatherSystem/Scripts/Generators.cs
+++ WeatherSystem/Assets/WeatherSystem/Scripts/Generators.cs
@@ -2,15 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generators : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public static class Generators
+{
 }
