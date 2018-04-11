b97841307a70bba86dec6019febe420eb758331d
Sam Lord <samuel.lord@students.plymouth.ac.uk>
2018-04-11 13:28:39 +0100

    Experimental changes to snow shader controller

--- WeatherSystem/Assets/WeatherSystem/Scripts/IntensityComponents/IntensityDrivenSnowShader.cs
+++ WeatherSystem/Assets/WeatherSystem/Scripts/IntensityComponents/IntensityDrivenSnowShader.cs
@@ -9,16 +9,18 @@ namespace WeatherSystem.IntensityComponents
         [SerializeField]
         private ScreenSpaceSnow snowController;
         [SerializeField]
-        [Range(0.0f,2.0f)]
+        [Range(0.0f,1.0f)]
         private float snowCumulationMultiplier = 0.1f;
         private float trackedIntensity = 0.0f;
 
         private int updateCount = 0;
+        private float timeActivated = 0.0f;
 
         public override void OnActivate()
         {
             trackedIntensity = 0.0f;
-            updateCount = 0;
+            updateCount = 1;
+            timeActivated = Time.timeSinceLevelLoad;
             snowController.Enable();
         }
 
@@ -33,9 +35,23 @@ namespace WeatherSystem.IntensityComponents
             {
                 updateCount++;
                 trackedIntensity += intensityData.intensity;
+                float timeSinceActivated = Time.timeSinceLevelLoad - timeActivated;
 
-                //snow value = average intensity over time * multiplier
-                snowController.BottomThreshold = 1.0f - (trackedIntensity / (float)updateCount) * snowCumulationMultiplier;
+                //snow value = average intensity per update by time * multiplier
+                float buildUpValue = (trackedIntensity / (float)updateCount) * timeSinceActivated * snowCumulationMultiplier;
+                snowController.BottomThreshold = 1.0f - buildUpValue;
+                
+                if(snowController.BottomThreshold == 0.0f) //Build up at max, so we start increasing top threshold
+                {
+                    snowController.TopThreshold = (trackedIntensity / (float)updateCount) * snowCumulationMultiplier;
+                }
+                else
+                {
+                    if(snowController.TopThreshold != 1.0f) //if top threshold isn't at min, need to decrease it when bottom threshold isn't at max
+                    {
+                        snowController.TopThreshold -= snowController.TopThreshold * 0.1f;
+                    }
+                }
             }
             else
             {
